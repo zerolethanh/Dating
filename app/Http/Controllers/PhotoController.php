1c5539a4bdb21b5f78c051aa958fa9769ed7046f
Nguyen Van Hai <coc.fuhn@gmail.com>
2016-12-10 15:42:22 +0900

    add some functions

--- app/Http/Controllers/PhotoController.php
+++ app/Http/Controllers/PhotoController.php
@@ -18,12 +18,50 @@ class PhotoController extends Controller
     {
         if ($file = request()->file('photo')) {
             $path = $file->storeAs('public', Uuid::uuid() . '.' . $file->guessClientExtension());
+
+            $this->getFaceAPI($path);
             return str_replace('public', 'storage', asset($path));
         }
 
         return [
             'msg' => 'file photo not found on request'
         ];
+    }
+    public function getFaceAPI($path){
+        $request = new Http_Request2('https://api.projectoxford.ai/face/v1.0/detect');
+        $url = $request->getUrl();
+
+        $headers = array(
+            // Request headers
+            'Content-Type' => 'application/json',
+            'Ocp-Apim-Subscription-Key' => '{2968a3fde555436aad8348f13a82a108}',
+        );
+
+        $request->setHeader($headers);
+
+        $parameters = array(
+            // Request parameters
+            'returnFaceId' => 'true',
+            'returnFaceLandmarks' => 'false',
+            'returnFaceAttributes' => '{age,gender}',
+        );
+
+        $url->setQueryVariables($parameters);
+
+        $request->setMethod(HTTP_Request2::METHOD_POST);
+
+// Request body
+        $request->setBody("{url:http://topanhdep.net/wp-content/uploads/2015/12/anh-girl-xinh-gai-dep-98-18.jpg}");
+
+        try
+        {
+            $response = $request->send();
+            echo $response->getBody();
+        }
+        catch (HttpException $ex)
+        {
+            echo $ex;
+        }
     }
 
 }
